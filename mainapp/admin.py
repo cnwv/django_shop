0059bd6fa6cc25b5da29ee0603d782d767e0cd37
cnwv <115669384+cnwv@users.noreply.github.com>
2023-07-19 22:11:41 +0400

    minor admin customisation

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 from mainapp.models import ProductCategory, Product
-admin.site.register(ProductCategory)
-admin.site.register(Product)
 
+admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'quantity')
+    fields = ('name', ('price', 'quantity',), 'image', 'description', 'short_description', 'category')
+    search_fields = ('name', 'category__name')
