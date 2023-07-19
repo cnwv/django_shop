36e8c507aff67f01b54b6c12f124bb299c688435
cnwv <115669384+cnwv@users.noreply.github.com>
2023-07-19 17:45:06 +0400

    add admin-users-read.html

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from adminapp.views import index
+from adminapp import views as admin
 
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', admin.index, name='index'),
+    path('admin-users-read/', admin.admin_users_read, name='admin_users_read')
 ]
