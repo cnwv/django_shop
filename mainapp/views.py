4efed4984bfe2bbd35f7f417153b883af96cc67b
cnwv <115669384+cnwv@users.noreply.github.com>
2023-09-26 02:31:27 +0400

    add filter by product's category

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,13 +12,9 @@ def index(request):
     return render(request, 'mainapp/index.html', title)
 
 
-def products(request, id=None):
-    title = {'title': 'catalog'}
+def products(request, category_id=None):
     context = {
-        'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
+        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
     }
     return render(request, 'mainapp/products.html', context)
-
-
-
