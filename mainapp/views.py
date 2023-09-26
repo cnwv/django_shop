ece4e9cdda0b4772d471fe7d297a4cce1f0ae813
cnwv <115669384+cnwv@users.noreply.github.com>
2023-09-26 18:22:05 +0400

    add product's pagination

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from mainapp.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -12,9 +13,20 @@ def index(request):
     return render(request, 'mainapp/index.html', title)
 
 
-def products(request, category_id=None):
-    context = {
-        'categories': ProductCategory.objects.all(),
-        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    }
+# def products(request, category_id=None):
+#     context = {
+#         'categories': ProductCategory.objects.all(),
+#         'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     }
+#     return render(request, 'mainapp/products.html', context)
+
+def products(request, category_id=None, page=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id).order_by('-price')
+    else:
+        products = Product.objects.all().order_by('-price')
+    paginator = Paginator(object_list=products, per_page=3)
+    products_paginator = paginator.page(page)
+    context = {'categories': ProductCategory.objects.all(), 'products': products_paginator}
     return render(request, 'mainapp/products.html', context)
+
