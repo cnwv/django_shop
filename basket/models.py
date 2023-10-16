3befc4e6c7b49cd59a0232fac79b1bbef3bf0e8a
cnwv <115669384+cnwv@users.noreply.github.com>
2023-10-16 15:31:21 +0400

    add dynamic pricing

--- basket/models.py
+++ basket/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from authapp.models import User
 from mainapp.models import Product
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+
+
+class BasketQuerySet(models.QuerySet):
+
+    def count_gt_2(self):
+        return self.filter(quantity__gt=2)
+
+    def count_lt_2(self):
+        return self.filter(quantity__lt=2)
+
+    def delete(self):
+        for object in self:
+            object.product.quantity += object.quantity
+            object.product.save()
+        super().delete()
 
 
 class Basket(models.Model):
@@ -8,6 +25,7 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
+    objects = BasketQuerySet.as_manager()
 
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
@@ -23,3 +41,23 @@ class Basket(models.Model):
         baskets = Basket.objects.filter(user=self.user)
         return sum(basket.sum() for basket in baskets)
 
+    def delete(self, using=None, keep_parents=False):
+        self.product.quantity += self.quantity
+        super().delete()
+
+    def save(self, *args):
+        if self.pk:
+            self.product.quantity -= self.quantity - Basket.objects.get(pk=self.pk).quantity
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super().save(*args)
+
+# @receiver(pre_save, sender=Basket)
+# def product_quantity_update(sender, update_fields, instance, **kwargs):
+#     if u
+#     if instance.pk:
+#         instance.product.quantity -= instance.quantity - instance.objects.get(pk=instance.pk).quantity
+#     else:
+#         instance.product.quantity -= instance.quantity
+#     instance.product.save()
